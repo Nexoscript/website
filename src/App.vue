1c22d1906c8db38322e47aaa38648e68ac60a8b7
ezTxmMC <eztxmmc@hotmail.com>
2025-02-04 18:01:35 +0100

    added base structure

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HeaderComp from '@/components/HeaderComp.vue'
+import FooterComp from '@/components/FooterComp.vue'
+</script>
+
+<template>
+  <HeaderComp />
+  <main></main>
+  <FooterComp />
+</template>
+
+<style scoped></style>
